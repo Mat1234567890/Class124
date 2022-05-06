fadddc63acc848c025abdeee94f71833b6c5087a
Mat1234567890 <78577538+Mat1234567890@users.noreply.github.com>
2022-05-05 21:21:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX = 0;
+noseY = 0;
+LeftWristX = 0;
+RightWristX = 0;
+difference = 0;
+function setup(){
+video = createCapture(VIDEO);
+video.size(550,500);
+canvas = createCanvas(300,300);
+canvas.position(560,150);
+poseNet = ml5.poseNet(video,modelLoaded);''
+poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+if (results.length > 0){
+console.log(results)
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("NoseX = " + noseX + " NoseY = " + noseY);
+RightWristX = results[0].pose.rightwrist.x;
+LeftWristX = results[0].pose.leftwrist.x;
+console.log("Right Wrist X = " + RightWristX + " Left Wrist X = " + LeftWristX);
+difference = floor(LeftWristX-RightWristX);
+console.log("Difference: " + difference);
+}
+}
+function modelLoaded(){
+console.log("Model Loaded!");
+}
+function draw(){
+background("#3C6FFF");
+document.getElementById("square_side").innerHTML = "Width and Height of the square will be: " + difference + " px";
+fill("#628BFF");
+stroke("#3166FA");
+square(noseX,noseY,difference);
+}
